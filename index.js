899e46827485d680c40ac05ec740647fa94da79f
muhammet-gogebakan <mgogebakan116@gmail.com>
2022-07-03 08:49:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//index.js dosyası
+
+// import name_x, {topla, cikar,  text, user, hello} from './my-module.js' //
+// console.log(topla(2, 4))
+// console.log(name_x)
+// console.log(cikar(2, 4))
+// console.log(text)
+// console.log(user)
+// hello("Muhammet")
+
+import getData from './app.js' //
+
+console.log(getData(1))
